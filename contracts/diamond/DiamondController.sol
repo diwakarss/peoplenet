339fed236ad6589afd2cee84b52a0cb36ca16d6f
Diwa <diwakarss@gmail.com>
2025-02-20 14:33:21 -0600

    v 0.0.1

---
+++ contracts/diamond/DiamondController.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { LibDiamond } from "./LibDiamond.sol";
+import { IDiamondCut } from "../interfaces/IDiamondCut.sol";
+import { IDiamondLoupe } from "../interfaces/IDiamondLoupe.sol";
+import { IERC173 } from "../interfaces/IERC173.sol";
+import { IERC165 } from "@openzeppelin/contracts/interfaces/IERC165.sol";
+
+contract DiamondController {
+    constructor(address _contractOwner, address _diamondCutFacet) payable {
+        require(_contractOwner != address(0), "Invalid owner address");
+        require(_diamondCutFacet != address(0), "Invalid diamond cut facet address");
+
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+        ds.contractOwner = _contractOwner;
+
+        // Add DiamondCutFacet
+        IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](1);
+        bytes4[] memory functionSelectors = new bytes4[](1);
+        functionSelectors[0] = IDiamondCut.diamondCut.selector;
+        
+        cut[0] = IDiamondCut.FacetCut({
+            facetAddress: _diamondCutFacet,
+            action: IDiamondCut.FacetCutAction.Add,
+            functionSelectors: functionSelectors
+        });
+
+        LibDiamond.diamondCut(cut, address(0), new bytes(0));
+
+        // Initialize supported interfaces
+        ds.supportedInterfaces[type(IERC165).interfaceId] = true;
+        ds.supportedInterfaces[type(IDiamondCut).interfaceId] = true;
+        ds.supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
+        ds.supportedInterfaces[type(IERC173).interfaceId] = true;
+    }
+
+    // Find facet for function that is called and execute the
+    // function if a facet is found and return any value.
+    fallback() external payable {
+        LibDiamond.DiamondStorage storage ds;
+        bytes32 position = LibDiamond.DIAMOND_STORAGE_POSITION;
+        assembly {
+            ds.slot := position
+        }
+
+        address facet = ds.selectorToFacetAndPosition[msg.sig].facetAddress;
+        require(facet != address(0), "Diamond: Function does not exist");
+
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), facet, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+
+    receive() external payable {}
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,59 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-contract-sizer");
+require("@matterlabs/hardhat-zksync-solc");
+require("@matterlabs/hardhat-zksync-deploy");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    polygon_mumbai: {
+      url: process.env.POLYGON_MUMBAI_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    zkSync_testnet: {
+      url: process.env.ZKSYNC_TESTNET_URL || "",
+      ethNetwork: "goerli",
+      zksync: true,
+    }
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  zksolc: {
+    version: "1.3.13",
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      }
+    }
+  }
 };
